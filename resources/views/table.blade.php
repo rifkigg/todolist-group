3cf2b5b76de62c213ded263522abfbccaea4ea9e
rifkigg <rifkiadi348@gmail.com>
2024-08-20 16:04:29 +0700

    template dashboard done

---
+++ resources/views/table.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.4/css/dataTables.bootstrap5.css">
+
+</head>
+<body>
+    <table id="example">
+        <thead>
+            <tr>
+                <th>nama</th>
+                <th>no</th>
+                <th>email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>ici</td>
+                <td>12</td>
+                <td>ici@gmail.com</td>
+            </tr>
+            <tr>
+                <td>ici</td>
+                <td>1</td>
+                <td>ici@gmail.com</td>
+            </tr>
+            <tr>
+                <td>ici</td>
+                <td>1</td>
+                <td>ici@gmail.com</td>
+            </tr>
+            <tr>
+                <td>ici</td>
+                <td>1</td>
+                <td>ici@gmail.com</td>
+            </tr>
+            <tr>
+                <td>ici</td>
+                <td>1</td>
+                <td>ici@gmail.com</td>
+            </tr>
+            <tr>
+                <td>ici</td>
+                <td>1</td>
+                <td>ici@gmail.com</td>
+            </tr>
+            <tr>
+                <td>ici</td>
+                <td>1</td>
+                <td>ici@gmail.com</td>
+            </tr>
+            <tr>
+                <td>ici</td>
+                <td>1</td>
+                <td>ici@gmail.com</td>
+            </tr>
+            <tr>
+                <td>ici</td>
+                <td>1</td>
+                <td>ici@gmail.com</td>
+            </tr>
+        </tbody>
+    </table>
+   <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+   <script src="https://cdn.datatables.net/2.1.4/js/dataTables.js"></script>
+   <script src="https://cdn.datatables.net/2.1.4/js/dataTables.bootstrap5.js"></script>
+   <script>new DataTable('#example');</script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/table', function () {
+    return view('table');
+});
+Route::get('/base', function () {
+    return view('pages.project');
+});
