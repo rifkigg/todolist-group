4635ff42c47ab9e5b00b41a9197a379ee170de97
athaarsley <athaarsley@gmail.com>
2024-08-21 11:41:28 +0700

    1

--- app/Http/Controllers/project_categories.php
+++ app/Http/Controllers/project_categories.php
@@ -5,27 +5,61 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+
 class project_categories extends Controller
 {
 
-    public function create(): View
+    public function index()
     {
-        return view('products.create');
+        $categories = project_categories::all();
+        return view('pages.project.categoriesProject', compact('categories'));
     }
 
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request)
     {
-        //validate form
+        // Validate and create a new project category
         $request->validate([
-            'name'         => 'required|min:5',
-            'slug'         => 'required|min:5'
+            'name' => 'required|string|max:255',
+            'slug' => 'required|string|max:255|unique:project_categories',
         ]);
+        return project_categories::create($request->all());
+    }
 
-        //create product
-        Product::create([
-            'name'         => $request->name,
-            'slug'         => $request->slug
+    public function create()
+    {
+        return view('project_categories.form');
+    }
+
+    public function show($id)
+    {
+        // Retrieve a specific project category by ID
+        return project_categories::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Validate and update the specified project category
+        $request->validate([
+            'name' => 'sometimes|required|string|max:255',
+            'slug' => 'sometimes|required|string|max:255|unique:project_categories,slug,' . $id,
         ]);
+        $category = project_categories::findOrFail($id);
+        $category->update($request->all());
+        return $category;
+    }
 
-}
+
+    public function edit($id)
+    {
+        $category = project_categories::findOrFail($id);
+        return view('project_categories.form', compact('category'));
+    }
+
+    public function destroy($id)
+    {
+        // Delete the specified project category
+        $category = project_categories::findOrFail($id);
+        $category->delete();
+        return response()->noContent();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\project_categories;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
