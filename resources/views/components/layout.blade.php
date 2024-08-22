c04f5a690cb92c877ac94f956ce2e2cd3c82295c
rifkigg <rifkiadi348@gmail.com>
2024-08-22 16:51:41 +0700

    ketinggalan ckeditor

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,7 +10,7 @@
     <meta name="author" content="">
 
     <title>Dashboard</title>
-    
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/2.1.4/css/dataTables.bootstrap5.css">
 
@@ -24,27 +24,31 @@
 </head>
 
 <body id="page-top">
-        {{ $slot }}
-        <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-        {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script> --}}
-        <script src="https://cdn.datatables.net/2.1.4/js/dataTables.js"></script>
-        <script src="https://cdn.datatables.net/2.1.4/js/dataTables.bootstrap5.js"></script>
-        <script>
-            new DataTable('#example');
-        </script>
-        <script src="https://kit.fontawesome.com/87dd173a0d.js" crossorigin="anonymous"></script>
-        <!-- Bootstrap core JavaScript-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-        </script>
+    {{ $slot }}
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script> --}}
+    <script src="https://cdn.datatables.net/2.1.4/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.1.4/js/dataTables.bootstrap5.js"></script>
+    <script>
+        new DataTable('#example');
+    </script>
+    <script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace('project_detail');
+    </script>
+    <script src="https://kit.fontawesome.com/87dd173a0d.js" crossorigin="anonymous"></script>
+    <!-- Bootstrap core JavaScript-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 
-        <!-- Bootstrap core JavaScript-->
-        <script src="{{ asset('/assets/vendor/jquery/jquery.min.js') }}"></script>
-        {{-- <script src="{{ asset('/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script> --}}
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('/assets/vendor/jquery/jquery.min.js') }}"></script>
+    {{-- <script src="{{ asset('/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script> --}}
 
-        <!-- Core plugin JavaScript-->
-        <script src="{{ asset('/assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-        <script src="{{ asset('/assets/js/sb-admin-2.min.js') }}"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="{{ asset('/assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('/assets/js/sb-admin-2.min.js') }}"></script>
 
 </body>
 
