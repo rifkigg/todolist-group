ac2d0b6702877a606e33489667695e766421c2f7
Levyantery <mangea312@gmail.com>
2024-08-22 08:42:52 +0700

    menambahkan pernyataan delete di status

--- resources/views/pages/project/statusProject.blade.php
+++ resources/views/pages/project/statusProject.blade.php
@@ -177,7 +177,8 @@
                                                     method="POST">
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger">Hapus</button>
+                                                    <button type="submit" class="btn btn-danger"
+                                                onclick="return confirm('Are you sure you want to delete this item?');">Delete</button>
                                                 </form>
 
                                             </td>
