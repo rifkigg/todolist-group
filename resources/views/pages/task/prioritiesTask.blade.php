21a8c5ec4c892f61ad329ce33de61a1ffe184ab5
athaarsley <athaarsley@gmail.com>
2024-08-23 15:16:03 +0700

    Task Priorities

--- resources/views/pages/task/prioritiesTask.blade.php
+++ resources/views/pages/task/prioritiesTask.blade.php
@@ -46,6 +46,7 @@
                             <a class="collapse-item " href="{{ route('task.index') }}">Task</a>
                             <a class="collapse-item" href="{{ route('status.index') }}">Task Status</a>
                             <a class="collapse-item " href="{{ route('priorities.index') }}">Task Priorities</a>
+                            <a class="collapse-item " href="{{ route('labels.index') }}">Task Labels/Tags</a>
                             
                         @elseif (auth()->user()->role == 'manajer')
                             <a class="collapse-item active" href="{{ route('task.index') }}">Task</a>
