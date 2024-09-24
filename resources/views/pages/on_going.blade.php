9cbfeea6b0ce4a2617c65516f4b2005ec18c8e65
Levyantery <mangea312@gmail.com>
2024-09-24 13:20:56 +0700

    mengubah created on going menjadi angka dan zona time count menjadi asia dan menghilangkan selected assignees di boards

--- resources/views/pages/on_going.blade.php
+++ resources/views/pages/on_going.blade.php
@@ -132,7 +132,7 @@
                                                         @else
                                                         @endif
                                                     </td>
-                                                    <td>{{ \Carbon\Carbon::parse($activeTask['created_at'])->format('d F Y') }}</td>
+                                                    <td>{{ \Carbon\Carbon::parse($activeTask['created_at'])->timezone('Asia/Jakarta')->format('d m Y H:i:s') }}</td>
                                                 </tr>
                                             @endforeach
                                         </tbody>
