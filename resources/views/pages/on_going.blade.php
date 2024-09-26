a2ae583717fc5ca2e2c8378e0478d7e4af8770ee
Lutttfi <lutfiofficial149@gmail.com>
2024-09-26 16:26:49 +0700

    perbaiki

--- resources/views/pages/on_going.blade.php
+++ resources/views/pages/on_going.blade.php
@@ -132,7 +132,7 @@
                                                         @else   
                                                         @endif
                                                     </td>
-                                                    <td>{{ \Carbon\Carbon::parse($activeTask['created_at'])->timezone('Asia/Jakarta')->format('d/m/Y H:i:s') }} </td>
+                                                    <td>{{ $activeTask['created_at'] }} </td>
                                                 </tr>
                                             @endforeach
                                         </tbody>
