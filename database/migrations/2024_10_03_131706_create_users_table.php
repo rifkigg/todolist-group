c1b15801e44330742d94e4cfad939dbb6d601c32
rifkigg <rifkiadi348@gmail.com>
2024-10-03 14:40:15 +0700

    bismillah

--- database/migrations/2024_10_03_131706_create_users_table.php
+++ database/migrations/2024_10_03_131706_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('role');
+            $table->enum('role', ['admin', 'developer', 'manajer', 'editor'])->default('admin');
             $table->rememberToken();
             $table->timestamps();
         });
